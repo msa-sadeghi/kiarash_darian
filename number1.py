bdc466ecc112563cfbd7164c61b5ec9bfc9f3441
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-03-11 20:58:54 +0330

    first session

--- number1.py
+++ number1.py
@@ -11,5 +11,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    pygame.draw.line(screen, (255, 0,0) , (100, 200), (100, 400))
+    pygame.draw.rect(screen, "green", (300, 300, 150, 150))
+    pygame.draw.rect(screen, "green", (50, 360, 150, 150), 4)
+    pygame.draw.ellipse(screen, "darkblue", (150, 460, 150, 150), 4)
     pygame.display.update()
---
+++ number2.py
@@ -0,0 +1,29 @@
+# import os
+# files_names_list = os.listdir('./knight')
+# for file_name in files_names_list:
+#     os.rename(f'./knight/{file_name}',
+#                 f'./knight/{file_name}'.replace(" ","").replace("(", "").replace(")", "")
+#                 )
+
+
+
+import pygame
+pygame.init()
+
+
+WIDTH = 1000
+HEIGHT = 600
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+player_image = pygame.image.load("./knight/Idle1.png")
+player_image = pygame.transform.scale_by(player_image,  0.5)
+player_rect = player_image.get_rect(topleft=(100,10))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.blit(player_image, player_rect)
+    pygame.draw.rect(screen, "purple", player_rect, 2)
+    pygame.display.update()
