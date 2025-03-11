bd438a6db7017ab57f7010c035daa62a9941b30e
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-03-11 20:09:58 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+
+
+WIDTH = 1000
+HEIGHT = 600
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
